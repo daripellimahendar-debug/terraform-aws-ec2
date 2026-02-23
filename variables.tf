a7a4a4bb56436908057e4a787ec655389debaca2
Mahendar d <daripellimahendar@gmail.com>
2026-02-23 14:08:08 +0530

    Add Terraform variables for AWS deployment

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS region for deployment"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.small"
+}
+
+variable "instance_name" {
+  description = "Name tag for the EC2 instance"
+  type        = string
+  default     = "my-ec2-instance"
+}
+
+variable "allowed_ssh_cidrs" {
+  description = "CIDR blocks allowed for SSH access"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
